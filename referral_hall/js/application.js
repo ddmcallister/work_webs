1d7f1c73b263469a014f1b0b5800c366ae4d126c
ddmcallister <destanie@gmail.com>
2015-01-07 23:22:34 -0500

    referral hall uglified

--- referral_hall/js/application.js
+++ referral_hall/js/application.js
@@ -1,9 +1,27 @@
-/*(function($){
+/*function test() { 
+var aD = document.getElementsByClassName("controls");
+console.log(aD);
+var b = [];
+	function innerFun() {
+    for(q = 0; q < aD.length; q++) {
+    b.push(aD[q].innerText);
+    console.log(b);
+}
+innerFun();
+    }
+}
+test();*/
+
+
+
+(function($){
 	function floatLabel(inputType){
 		$(inputType).each(function(){
 			var $this = $(this);
 
 			$this.focus(function(){
+				alert( "5 to 1." );
+
 				$this.next().addClass("active");
 			});
 			//on blur check field and remove class if needed
@@ -16,17 +34,17 @@
 	}
 	// just add a class of "floatLabel to the input field!"
 	floatLabel(".floatLabel");
-})(jQuery);*/
+});
 
-window.onload = function() {
+/*window.onload = function() {
 	var seal = document.getElementById("seal");
 	TweenLite.to(seal, 2, {left:"300px"});
 	//TweenLite.to(seal, 2, {rotation:"-360", transformOrigin:"0% 100%"});
-}
+}*/
 
-var test = document.getElementById("name");
+/*var test = document.getElementById("name");
 name.onfocus = function() {
 	var namemove = document.getElementById("lname");
 
 	TweenLite.to(namemove, 2, {rotation:"-360", transformOrigin:"0% 100%"});
-}
+}*/
