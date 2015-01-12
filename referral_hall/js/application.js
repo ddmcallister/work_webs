d31b940adaaa34fc8b76458cc7676f7bdd9eb537
ddmcallister <destanie@gmail.com>
2015-01-12 07:59:01 -0500

    rh - changed colors

--- referral_hall/js/application.js
+++ referral_hall/js/application.js
@@ -1,7 +1,6 @@
 function main() {
 
 	$('.floatlabel').focus(function() {
-		//$(this).off('hover');
 		$(this).addClass('test');
 		$(this).next().addClass('active');
 	});
@@ -14,17 +13,3 @@ function main() {
 }
 
 $(document).ready(main);
-
-
-/*window.onload = function() {
-	var seal = document.getElementById("seal");
-	TweenLite.to(seal, 2, {left:"300px"});
-	//TweenLite.to(seal, 2, {rotation:"-360", transformOrigin:"0% 100%"});
-}*/
-
-/*var test = document.getElementById("name");
-name.onfocus = function() {
-	var namemove = document.getElementById("lname");
-
-	TweenLite.to(namemove, 2, {rotation:"-360", transformOrigin:"0% 100%"});
-}*/
