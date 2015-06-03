5a4e3055e37fcd2de15e6343ab3f2cc42b77be37
ddmcallister <destanie@gmail.com>
2015-06-02 20:18:16 -0400

    sample pa page more responsive

--- raff_becker_newstyle/js/application.js
+++ raff_becker_newstyle/js/application.js
@@ -11,6 +11,25 @@ function main() {
 			$('#employee_index').removeClass('invis');
 		});
 
+
+    $('.top_nav_wr').burgerMenu({
+		buttonBg: '#ccb261', 
+		buttonWidth: '50px',
+		buttonHeight: '50px',
+		linkSelector: 'a',
+		linkText: function($a){ return $a.text() },
+		fixed: true, // set to true if the burger menu has to follow the window scroll
+		hideInitialNav: true,
+		animateSpeed: 0,
+		hideOnBodyClick: true,
+		showUntilWidth: 640,
+		top: 0,
+		lineColor: '#83362c',
+		position: 'right',
+		overlay: '#ccb261'
+    });
+
+
 }
 
 $(document).ready(main);
