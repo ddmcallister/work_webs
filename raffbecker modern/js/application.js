402faf185af8ee177920f51fb4fd36d0f423123b
ddmcallister <destanie@gmail.com>
2015-06-22 07:20:40 -0400

    rb home page

--- raffbecker modern/js/application.js
+++ raffbecker modern/js/application.js
@@ -1,3 +1,27 @@
+function main() {
+
+	$('#employer-index').hover(
+		function() {
+			$('#employer-pa').removeClass('invis');
+			$('#employer-pa').addClass('pa');
+			$('#employee-index').addClass('invis');
+		}, function () {
+			$('#employer-pa').addClass('invis');
+			//$('#employer_pa').addClass('pa');
+			$('#employee-index').removeClass('invis');
+		});
+
+		$('#employee-index').hover(
+		function() {
+			$('#employee-pa').removeClass('invis');
+			$('#employee-pa').addClass('pa');
+			$('#employer-index').addClass('invis');
+		}, function () {
+			$('#employee-pa').addClass('invis');
+			//$('#employer_pa').addClass('pa');
+			$('#employer-index').removeClass('invis');
+		});
+
 $("a").hover(
 	function() {
   		$(this).css("color", "#ffffff");
@@ -5,3 +29,10 @@ $("a").hover(
 	}, function() {
 		$(this).css("color", "#FF8C16");
 });
+
+}
+
+$(document).ready(main);
+
+
+
