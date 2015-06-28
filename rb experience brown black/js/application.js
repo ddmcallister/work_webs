f6316a41e73c031b4b8844d5aa01803080549fa2
ddmcallister <destanie@gmail.com>
2015-06-28 16:42:54 -0400

    lovely law firm page

--- rb experience brown black/js/application.js
+++ rb experience brown black/js/application.js
@@ -1,15 +1,37 @@
 function main() {
-var i = 0;
+	$("#attorney-square [id*='index'] img").hover(function() {
+		$(this).next().css('display', 'inline');
+	}, function () {
+		$(this).next().css('display', 'none');
+	}
+
+		);
+
+/*var i = 0;
 $('ul').each(function() {
-    var hide_after = $(this).children().length;
+    //var hide_after = $(this).children().length;
     $(this).children().each(function(counter) {
-        $(this)
+
             .delay(++i * 2000)
-            .animate({left:0, opacity:1})
-            .delay((hide_after - counter) * 2000)
-            .animate({left:'100%', opacity: 0});
+            .animate({top:0, opacity:1})
+            //.delay((hide_after - counter) * 2000)
+			.delay(2000)
+            .animate({bottom:'100%', opacity: 0});
     });
-});
-}
+});*/
+/*	var i = $('li').length;
+	 $('li').each(function() {
+		if ($(this.i).css("opacity", "0")) {
+			$(this.i).css("opacity", "1");
+		} else {
+			$(this.i).css("opacity", "0");
+		}
+	// 	$('li'.i).fadeIn();
+	// 	$('li'.i).fadeIn();
+	// 	//$(this).fadeIn();
+ //        //$(this).fadeOut();
+	// 	});
+});*/
+	}
 
 $(document).ready(main);
