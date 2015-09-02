86daa201471f546623948d29e431527ede517471
ddmcallister <destanie@gmail.com>
2015-09-02 09:09:44 -0400

    some show hide movement on home page

--- rb experience/js/application.js
+++ rb experience/js/application.js
@@ -1,28 +1,24 @@
 function main() {
-
-/*	$("#attorney-square [id*='index'] img").hover(function() {
-			$(this).next().css({'display': 'block', 'background': '#BE3100', 'width': '250px'});
-		}, function () {
-			$(this).next().css('display', 'none');
-		}
-	);*/
-
-	var j = 0;
-	var delay = 4000;
-	function cycleThru(){
-	     var jmax = $("ul#pa-flip li").length -1;
-	     $("ul#pa-flip li:eq(" + j + ")")
-	        .animate({"opacity": "1", "height": "2em"}, 600)
-	        .animate({"opacity" : "1", "height": "2em"}, delay)
-	        .animate({"opacity" : "0", "height": "0"}, 600, function(){
-	            (j == jmax) ? j=0 : j++;
-	            cycleThru();
-	        });
-	};
+	window.setTimeout(function () {
+		document.getElementById("index-text").style.display = "block";
+		document.getElementById("pa-examples").style.display = "inline-block";
+		document.getElementById("index-exp").setAttribute("style", "font-size: 300%; margin: 0.5em 0 0 70%;");
+		document.getElementById("page-break-a").style.display = "block";
+		var j = 0;
+		var delay = 3000;
+		function cycleThru(){
+	     	var jmax = $("ul#pa-flip li").length -1;
+	     	$("ul#pa-flip li:eq(" + j + ")")
+	        	.animate({"opacity": "1", "height": "3em"}, 600)
+	        	.animate({"opacity" : "1", "height": "3em"}, delay)
+	        	.animate({"opacity" : "0", "height": "0"}, 600, function(){
+	            	(j == jmax) ? j=0 : j++;
+	            	cycleThru();
+	        	});
+		};
 	cycleThru();
 
- 
-
+	}, 4000); 
 }
 
 $(document).ready(main);
