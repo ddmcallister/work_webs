843eb9dd4103481eb4c5737f33fc7f8289044eb4
ddmcallister <destanie@gmail.com>
2015-09-05 15:03:36 -0400

    changed animated element

--- rb experience/js/application.js
+++ rb experience/js/application.js
@@ -1,16 +1,21 @@
 function main() {
-	window.setTimeout(function () {
-		document.getElementById("index-text").style.display = "block";
-		document.getElementById("pa-examples").style.display = "inline-block";
-		document.getElementById("index-exp").setAttribute("style", "font-size: 300%; margin: 0.5em 0 0 70%;");
-		document.getElementById("page-break-a").style.display = "block";
+	/*window.setTimeout(function () {
+		var x = document.getElementsByClassName("text-block");
+		var i;
+		for (i = 0; i < x.length; i++) {
+    		x[i].style.display = "block";
+		}
+		//document.getElementsByClassName("text-block").style.display = "block";
+		// document.getElementById("pa-examples").style.display = "inline-block";
+		//document.getElementById("index-exp").setAttribute("style", "font-size: 300%; margin: 0.5em 0 0 70%;");
+		document.getElementById("page-break-a").style.display = "block";*/
 		var j = 0;
-		var delay = 3000;
+		var delay = 2000;
 		function cycleThru(){
 	     	var jmax = $("ul#pa-flip li").length -1;
 	     	$("ul#pa-flip li:eq(" + j + ")")
-	        	.animate({"opacity": "1", "height": "3em"}, 600)
-	        	.animate({"opacity" : "1", "height": "3em"}, delay)
+	        	.animate({"opacity": "1", "height": "20em"}, 600)
+	        	.animate({"opacity" : "1", "height": "20em"}, delay)
 	        	.animate({"opacity" : "0", "height": "0"}, 600, function(){
 	            	(j == jmax) ? j=0 : j++;
 	            	cycleThru();
@@ -18,7 +23,7 @@ function main() {
 		};
 	cycleThru();
 
-	}, 4000); 
+	//}, 4000); 
 }
 
 $(document).ready(main);
